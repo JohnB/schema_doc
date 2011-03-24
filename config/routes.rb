f4ebb77b3416ccc889c8bf4d22969e9f1d9b0286
John Baylor <john.baylor@gmail.com>
2011-03-24 15:30:56 -0700

    Fixed pluralization of routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do |map|
-  map.resources :schema_doc, :except => [:new, :create, :edit, :destroy]
+  map.resources :schema_docs, :except => [:new, :create, :edit, :destroy]
 end
